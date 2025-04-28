4b45fa7cb064a8c85f85b1ff167d6b0d62b76b57
lesnijahoda <honzabro137@seznam.cz>
2025-04-28 18:58:25 +0200

    Lepší načtění background images

--- res/js/backgrounds.js
+++ res/js/backgrounds.js
@@ -2,39 +2,40 @@ export function renderBackgroundPanel(hero) {
   const backgroundOptions = document.getElementById('backgroundOptions');
   const toggleButton = document.getElementById('toggleBackgroundPanel');
 
-  // Ošetření pro případ, že se něco nenačte 
+   
   if (!backgroundOptions || !toggleButton) {
     console.error('backgroundOptions nebo toggleButton nebyl nalezen!');
     return;
   }
 
-  // Otevření a zavření panelu
+  // otevření a zavření panelu
   toggleButton.addEventListener('click', () => {
     backgroundOptions.style.display = backgroundOptions.style.display === 'none' ? 'block' : 'none';
   });
 
-  // Naplnění tlačítek s backgrounds 
-  backgroundOptions.innerHTML = `
-    <button data-image="./res/img/backgrounds/louka.avif" data-cost="0">Pozadí Louka (0 zlata)</button>
-    <button data-image="./res/img/backgrounds/city.png" data-cost="100">Pozadí Vesmír (10000 zlata)</button>
-    <button data-image="./res/img/backgrounds/night.jpg" data-cost="50">Pozadí Měsíček (50 zlata)</button>
-    <button data-image="./res/img/backgrounds/vesmir.png" data-cost="100">Pozadí Vesmír (100 zlata)</button>
-    <button data-image="./res/img/backgrounds/les.jpg" data-cost="100">Pozadí Vesmír (10000 zlata)</button>
-    <button data-image="./res/img/backgrounds/theFurther.jpg" data-cost="100">Pozadí Vesmír (10000 zlata)</button>
-    <button data-image="./res/img/backgrounds/village.jpg" data-cost="100">Pozadí Vesmír (10000 zlata)</button>
-    <button data-image="./res/img/backgrounds/creation.jpg" data-cost="100">Pozadí Vesmír (10000 zlata)</button>
-   
-    <button data-image="./res/img/backgrounds/skyCity.png" data-cost="100">Pozadí Vesmír (10000 zlata)</button>
-    
-    
-  `;
+  //pole s backgrounds
+  const backgrounds = [
+    { name: "Pozadí Louka", image: "./res/img/backgrounds/louka.avif", cost: 0 },
+    { name: "Pozadí Vesmír", image: "./res/img/backgrounds/city.png", cost: 50 },
+    { name: "Pozadí Měsíček", image: "./res/img/backgrounds/night.jpg", cost: 300 },
+    { name: "Pozadí Vesmír", image: "./res/img/backgrounds/vesmir.png", cost: 1000 },
+    { name: "Pozadí Les", image: "./res/img/backgrounds/les.jpg", cost: 10000 },
+    { name: "Pozadí The Further", image: "./res/img/backgrounds/theFurther.jpg", cost: 10500 },
+    { name: "Pozadí Village", image: "./res/img/backgrounds/village.jpg", cost: 100000 },
+    { name: "Pozadí Creation", image: "./res/img/backgrounds/creation.jpg", cost: 105000 },
+    { name: "Pozadí Sky City", image: "./res/img/backgrounds/skyCity.png", cost: 110000 }
+  ];
 
-  // Přiřazení funkčnosti ke každému tlačítku
-  backgroundOptions.querySelectorAll('button').forEach(button => {
+  
+  backgroundOptions.innerHTML = backgrounds.map(background => 
+    `<button data-cost="${background.cost}">${background.name} (${background.cost} zlata)</button>`
+  ).join('');
+
+  
+  backgroundOptions.querySelectorAll('button').forEach((button, index) => {
     button.addEventListener('click', () => {
-      const imagePath = button.getAttribute('data-image');
-      const cost = parseInt(button.getAttribute('data-cost'), 10);
-      buyBackground(hero, imagePath, cost);
+      const background = backgrounds[index];
+      buyBackground(hero, background.image, background.cost);
     });
   });
 }
